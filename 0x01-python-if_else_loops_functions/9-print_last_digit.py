aad15dd275ec5790e84c37c0139ccf3654d58ee2
kateincoding <kateincoding@gmail.com>
2021-04-27 23:45:44 +0000

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = number % 10
+    else:
+        last_digit = number * -1 % 10
+    print("{:d}".format(last_digit), end="")
+    return last_digit
