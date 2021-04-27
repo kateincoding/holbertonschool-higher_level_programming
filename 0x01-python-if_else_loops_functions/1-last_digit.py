d840337aa10629339dde8ad9aaa5fc94868cabb3
kateincoding <kateincoding@gmail.com>
2021-04-27 20:13:01 +0000

    fix error in negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
 else:
-    last_digit = (number % 10) * (-1)
+    last_digit = (number * -1 % 10) * -1
 
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
