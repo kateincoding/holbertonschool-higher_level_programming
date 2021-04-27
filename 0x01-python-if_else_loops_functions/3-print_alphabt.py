823c2cf2ed0c0d435e11cd6bf344a93f7e13a2f4
kateincoding <kateincoding@gmail.com>
2021-04-27 21:58:33 +0000

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(97,123):
-    if c != 101 and c != 113: #not e neither q
-        print("{:c}".format(c), end="")
+	if c != 101 and c != 113: #not e neither q
+		print("{:c}".format(c), end="")
