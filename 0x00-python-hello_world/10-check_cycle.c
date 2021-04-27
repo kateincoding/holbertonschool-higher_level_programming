8fc3d91c7c7d3450d8254541404657d882feca66
kateincoding <kateincoding@gmail.com>
2021-04-27 02:41:36 +0000

    algorythm to check if it is a cycle in linked list or not

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - function that detects if a linked list is a cycle
+ * @list: linked list to evaluate
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *list_fast, *list_slow;
+
+	if (!list)
+		return (0);
+
+	list_fast = list;
+	list_slow = list;
+	while (list_fast)
+	{
+		list_fast = (list_fast->next)->next;
+		if (list_slow == list_fast)
+			return (1);
+		list_slow = list_slow->next;
+	}
+	return (0);
+}
