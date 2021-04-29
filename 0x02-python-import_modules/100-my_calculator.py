07f23b3a945bd77768fdff8bdde2e71fd9df845a
kateincoding <kateincoding@gmail.com>
2021-04-29 11:28:39 +0000

    exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         op = operators[i][0]
         if argv[2] == op:
             print("{} + {} = {}".format(a, b, operators[i][1](a, b)))
-            return 0
+            exit(0)
         i += 1
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
