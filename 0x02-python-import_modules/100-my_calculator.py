d30f18307d25545645a3d08ecd6ce2231a728e3b
kateincoding <kateincoding@gmail.com>
2021-04-29 11:25:12 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    argc = len(argv)
+    if (argc != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    i = 0
+    a = int(argv[1])
+    b = int(argv[3])
+    operators = [('+', add), ('-', sub), ('*', mul), ('/', div)]
+    while (i <= 3):
+        op = operators[i][0]
+        if argv[2] == op:
+            print("{} + {} = {}".format(a, b, operators[i][1](a, b)))
+            return 0
+        i += 1
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
