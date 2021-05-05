915f373ce4d60e5a03c45e24c564029e9e9bff82
kateincoding <kateincoding@gmail.com>
2021-05-05 00:07:20 +0000

    erasing the c and return a copy of string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     str_list = list(my_string)
     for i in str_list:
-        if (i == 'C' or i == 'c'):
+        if i == 'C' or i == 'c':
             str_list.remove(i)
     return("".join(str_list))
