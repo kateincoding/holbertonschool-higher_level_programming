ad320f72bd478df7870aa17106916e89788c8bea
kateincoding <kateincoding@gmail.com>
2021-05-05 00:32:24 +0000

    casting to i++

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str_list = list(my_string)
-    for i in str_list:
+    for i in str_list[1:]:
         if i == 'C' or i == 'c':
             str_list.remove(i)
     return("".join(str_list))
