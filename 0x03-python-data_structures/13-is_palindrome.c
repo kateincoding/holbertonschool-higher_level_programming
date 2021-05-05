b09cd0efc457bb6c84c49708d6f4e258807ee158
kateincoding <kateincoding@gmail.com>
2021-05-05 03:59:09 +0000

    function that check if it is palindromic with recursivity

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,35 @@
 #include "lists.h"
 
+/**
+ * _compare_extreme_nodes - function that use a recursion to check palindromic
+ * @head: head of linked list
+ * @tail: the node that will be the tail of a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int _compare_extreme_nodes(listint_t **head, listint_t *tail)
 {
-    int result;
+	int result;
 
-    if (!tail)
-        return (1);
+	if (!tail)
+		return (1);
 
-    result = _compare_extreme_nodes(head, tail->next);
-    result = ((*head)->n == tail->n);
-    *head = (*head)->next;
+	result = _compare_extreme_nodes(head, tail->next);
+	result = ((*head)->n == tail->n);
+	*head = (*head)->next;
 
-    return(result);
+	return (result);
 }
 
-
+/**
+ * is_palindrome - is palindrome a simply linked list without using a while
+ *
+ * @head: head of linked list
+ * Return: call another function to call the last node in less time
+ * 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-    if (!head)
-        return (0);
-    return (_compare_extreme_nodes(head, *head));
+	if (!head)
+		return (0);
+	return (_compare_extreme_nodes(head, *head));
 }
