ad91b8a325424106d5a35c87832560d7701c6fd9
kateincoding <kateincoding@gmail.com>
2021-05-05 00:34:59 +0000

    firs character c fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str_list = list(my_string)
-    for i in str_list[1:]:
+    for i in str_list[0:]:
         if i == 'C' or i == 'c':
             str_list.remove(i)
     return("".join(str_list))
