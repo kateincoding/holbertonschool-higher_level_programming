9e69f7574490a1d70a27fabfa8d051fba86b7f97
kateincoding <kateincoding@gmail.com>
2021-05-05 01:00:33 +0000

    casting to the counter = counter + 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
+    for i in matrix[0:]:
+        for j in i[0:]:
             print(" {:d}".format(j), end="")
         print()
