accbf3b5b30eae2d93fcdeb6c905a68ccf838862
kateincoding <kateincoding@gmail.com>
2021-05-05 00:53:34 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        space = ""
         for j in i:
             print(" {:d}".format(j), end="")
         print()
