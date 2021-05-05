7d47cfea3d189c45a3b2f2908a70c4df214de7c3
kateincoding <kateincoding@gmail.com>
2021-05-05 01:02:49 +0000

    erasing spaces in position 0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix[0:]:
+        spc = ""
         for j in i[0:]:
-            print(" {:d}".format(j), end="")
+            print("{:s}{:d}".format(spc, j), end="")
+            spc = " "
         print()
