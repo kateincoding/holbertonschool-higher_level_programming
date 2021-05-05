9e69db20224ae0349c33816aa5fd3722f6bbc414
kateincoding <kateincoding@gmail.com>
2021-05-05 04:55:52 +0000

    prunt python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints info about Python lists
+ * @p: Python Object
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+void print_python_list_info(PyObject *p)
+{
+	return ;
+}
