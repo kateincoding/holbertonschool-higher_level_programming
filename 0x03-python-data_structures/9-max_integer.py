6f4bed409d3277a06f08abc8da39dca59008ae4b
kateincoding <kateincoding@gmail.com>
2021-05-05 01:50:38 +0000

    max integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    tmp = my_list[0]
+    for i in my_list:
+        if i > tmp:
+            tmp = i
+
+    return tmp
