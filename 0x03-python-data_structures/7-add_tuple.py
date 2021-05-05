c1a80f08221599bf5450e90e1358f49cd20bdbf5
kateincoding <kateincoding@gmail.com>
2021-05-05 01:28:34 +0000

    pep8 style , given spaces for the comments

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    #give the format of a list[2]
-    #tuple_a
+    # give the format of a list[2]
+    # tuple_a
     if len(tuple_a) == 1:
         tuple_a = (tuple_a[0], 0)
     elif len(tuple_a) == 0:
         tuple_a = (0, 0)
-    #tuple_b
+    # tuple_b
     if len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
     elif len(tuple_b) == 0:
         tuple_b = (0, 0)
-    #add operation
+    # add operation
     tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (tuple_sum)
