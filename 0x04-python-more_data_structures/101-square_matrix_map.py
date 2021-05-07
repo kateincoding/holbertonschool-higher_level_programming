a81e3ecc104365faf7c4459b7f7d3c768ad1705b
kateincoding <kateincoding@gmail.com>
2021-05-07 03:07:10 +0000

    erase comments

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    # pow_column = lambda column: column**2
-    # pow_row = lambda row: list(map(pow_column, row))
     return (list(map(lambda row: list(map(lambda col: col**2, row)), matrix[:])))
