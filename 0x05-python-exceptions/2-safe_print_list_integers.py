583848c4defb37497261d01ea001c72d76bf538d
kateincoding <kateincoding@gmail.com>
2021-05-17 18:12:19 +0000

    without comments

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,11 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             number = number + 1
-        # except Exception as ex:
-        #    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        #    message = template.format(type(ex).__name__, ex.args)
-        #    print(message)
-        #    pass
         except (ValueError, TypeError):
             pass
     print()
