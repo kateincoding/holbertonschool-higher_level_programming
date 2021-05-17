37c6636bac64a93cd9266149fd132ff41a70c952
kateincoding <kateincoding@gmail.com>
2021-05-17 18:57:09 +0000

    retire unnecesary lines

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,20 +4,16 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             div = int(my_list_1[i]) / int(my_list_2[i])
-        except(TypeError, ValueError):
+        except(ValueError):
             print("wrong type")
             div = 0
-            continue
         except(ZeroDivisionError):
             print("division by 0")
             div = 0
-            continue
         except(IndexError):
             print("out of range")
             div = 0
-            continue
         finally:
             result.append(div)
-            pass
 
     return (result)
