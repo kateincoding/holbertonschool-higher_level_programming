1216536c568d24b4405348d122784a020aef8ca1
kateincoding <kateincoding@gmail.com>
2021-05-18 21:52:03 +0000

    handling comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" class Square """
+"""class Square"""
 
 class Square:
-    """ Definition of the empty class Square """
+    """Definition of the empty class Square"""
     pass
