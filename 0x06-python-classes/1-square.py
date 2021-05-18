0b9fc9486e1a33e35fabfc3157e943d8824561e8
kateincoding <kateincoding@gmail.com>
2021-05-18 22:04:59 +0000

    handling comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Class Square definition"""
+
+
 class Square:
-    """ Class Square that defines a square:
+    """Class Square that defines a square:
         - Private instance attribute: size
         - Instantiation with size (no type/value verification)
-        - You are not allowed to import any module """
+        - You are not allowed to import any module"""
     def __init__(self, size):
-        """ Size is a private attributte:
-        The size of a square is crucial for a square """
+        """Size is a private attributte:
+        The size of a square is crucial for a square"""
         self.__size = size
