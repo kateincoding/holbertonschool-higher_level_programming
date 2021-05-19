321f17389cfb89a4c3dd1cfbf0eabe172e39c6ea
kateincoding <kateincoding@gmail.com>
2021-05-19 03:54:12 +0000

    bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import math
+"""Class MagicClass"""
+
+
+class MagicClass:
+    """Magic class"""
+
+    def __init__(self, radius=0):
+        """ Init """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """ Area """
+        return(self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """ Circ """
+        return (2 * math.pi * self.__radius)
