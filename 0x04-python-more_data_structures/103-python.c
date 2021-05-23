7db8a0b4d1b58569cbf75ec33579a2c469e285c2
kateincoding <kateincoding@gmail.com>
2021-05-23 15:28:39 +0000

    create a library to list lists and by object

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,11 +1,35 @@
 #include <Python.h>
 
+void print_python_bytes(PyObject *p)
+{
+	int i = 0, bsize = 0;
+	char *str;
+
+	if (!PyBytes_Check(p))
+	{
+		printf("[ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	bsize = PyBytes_Size(p);
+    printf("[.] bytes object info\n");
+	printf("  size: %d\n", bsize);
+	str = ((PyBytesObject *)p)->ob_sval;
+	printf("  trying string: %s\n", str);
+	printf("  first 10 bytes:");
+
+	for (i = 0; (i <= bsize) && (i <= 10); i++)
+	{
+			printf("  %02hhx", str[i]);
+	}
+	printf("\n");
+}
+
 /**
  * print_python_list_info - prints info about Python lists
  * @p: Python Object
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-void print_python_list_info(PyObject *p)
+void print_python_list(PyObject *p)
 {
 	long int list_size, alloc_list, i;
 	PyObject *item;
@@ -13,6 +37,7 @@ void print_python_list_info(PyObject *p)
 	list_size = PyList_Size(p);
 	alloc_list = ((PyListObject *)p)->allocated;
 
+	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", list_size);
 	printf("[*] Allocated = %ld\n", alloc_list);
 	for (i = 0; i < list_size; i++)
@@ -22,9 +47,4 @@ void print_python_list_info(PyObject *p)
         if (PyBytes_Check(item))
             print_python_bytes(item);
 	}
-}
-
-void print_python_bytes(PyObject *p)
-{
-
 }
