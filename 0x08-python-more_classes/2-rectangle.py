4af10e1214f7f3b0b0fa8b97025c4e3ff431762a
kateincoding <kateincoding@gmail.com>
2021-05-24 16:30:58 +0000

    calculates the area and perimeter with a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+    """Constructor of the empty class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """property for private instance attribute: width"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """property for private instance attribute: height"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Method to calculate the area of Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Method to calculate the perimeter of Rectangle"""
+        if self.width <= 0 or self.height <= 0:
+            return 0
+        return 2 * (self.width + self.height)
