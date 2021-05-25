b31e7ff77faf5c387057ff3fdc7ce0c68ee4aca3
kateincoding <kateincoding@gmail.com>
2021-05-25 21:32:25 +0000

    Revert "answers of cpython"
    This reverts commit 2613d56748e1bb0c9e7d704fbb89313b50e65949.

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
     raise TypeError
-
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""class LockedClass"""
-
-
-class LockedClass:
-    """Class of LockedClass, statically allocated"""
-    __slots__ = "first_name"
