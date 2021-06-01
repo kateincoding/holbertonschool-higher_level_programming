7c260866524d00f7bde2f7f9707db221dd660d92
kateincoding <kateincoding@gmail.com>
2021-06-01 22:47:55 +0000

    if is inheritence of some object

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance of
+    a class that inherited (directly or indirectly) from the
+    specified class ; otherwise False."""
+    if a_class == type(obj):
+        return False
+    return isinstance(obj, a_class)
