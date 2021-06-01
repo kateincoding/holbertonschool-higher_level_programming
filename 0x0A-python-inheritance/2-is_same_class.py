a33c89053d227e181109c82e935070a892b3d9fd
kateincoding <kateincoding@gmail.com>
2021-06-01 22:27:45 +0000

    type boolean

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,15 +3,6 @@
 
 def is_same_class(obj, a_class):
     if type(obj) is a_class:
-        return (a_class)
+        return (True)
     else:
         return (False)
-
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
