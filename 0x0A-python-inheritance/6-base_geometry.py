d179b889c7b19f6a8268efbae5ce6cf27fa81ba0
kateincoding <kateincoding@gmail.com>
2021-06-01 23:06:16 +0000

    raise exception of not yet implemented function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty Class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Constructor of the class"""
+
+    def area(self):
+        """Function to calculate area that is not yet implemented"""
+        raise Exception("area() is not implemented")
