c4a42123f11698793555f0f69806ccb7b8ffab61
kateincoding <kateincoding@gmail.com>
2021-06-01 22:49:04 +0000

    pep8

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def lookup(obj):
     """function that returns the list of available
     attributes and methods of an object:"""
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,8 @@
 
 
 def is_same_class(obj, a_class):
+    """function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False"""
     if type(obj) is a_class:
         return (True)
     else:
