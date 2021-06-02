fb419520f4e6a15961aaa37fa4ed4c61e245c0ca
kateincoding <kateincoding@gmail.com>
2021-06-02 00:15:07 +0000

    retire a flag

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Class BaseGeometry and Class Rectangle"""
+"""Class definition"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Constructor of the Class Rectable"""
+    """Class Rectable"""
 
     def __init__(self, width, height):
-        """Initialization of width and height"""
+        """Constructor of Initialization of width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
@@ -21,5 +21,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Builtin str that will executes when print method is used"""
-        print("flag")
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
