1c80f9c070128a6af46be0294aad5b711ac4ed1f
kateincoding <kateincoding@gmail.com>
2021-06-02 02:11:34 +0000

    pep8

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"Methods' Module"
 
 
 def add_attribute(object, key, value):
     """Method to add new attribute if it is possible=type(dict)"""
 
-    if hasattr(object, "__dict__") == False:
+    if hasattr(object, "__dict__") is False:
         raise TypeError("can't add new attribute")
     setattr(object, key, value)
-
-class MyClass():
-    pass
