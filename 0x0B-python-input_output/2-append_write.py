49f56ccbaf35fba8e6ad59c312272eb12104bc71
kateincoding <kateincoding@gmail.com>
2021-06-02 12:39:43 +0000

    append at the final

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"Method Module"
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        return(f.write(text))
