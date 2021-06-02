a434b2773789af893da54facf5365a5b3b1bc549
kateincoding <kateincoding@gmail.com>
2021-06-02 00:18:44 +0000

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class BaseGeometry and Class Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Rectable"""
+
+    def __init__(self, size):
+        """Constructor: Initialization of width and height"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method: area calculation"""
+        return(super().area())
+
+    def __str__(self):
+        """Builtin str that will executes when print method is used"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
