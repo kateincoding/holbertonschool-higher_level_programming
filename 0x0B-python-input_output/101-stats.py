53a35eb4427cc97bfacf782750c38b2e0aff02a4
kateincoding <kateincoding@gmail.com>
2021-06-03 03:23:24 +0000

    verify format

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """Module to write a script of dict of Filesize and code_status"""
-
-
 import sys
 
 
 filesize = 0
 count = 0
-stc = {"200": 0,
-       "301": 0,
-       "400": 0,
-       "401": 0,
-       "403": 0,
-       "404": 0,
-       "405": 0,
-       "500": 0}
-
+stc = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 try:
     for line in sys.stdin:
         line = line.split()
@@ -37,12 +28,12 @@ try:
         if count % 10 == 0:
             print("File size: {}".format(filesize))
             for key in sorted(stc.keys()):
-                if stc[key] > 0:
+                if stc[key] != 0:
                     print("{}: {}".format(key, stc[key]))
 
 except KeyboardInterrupt:
     print("File size: {}".format(filesize))
     for key in sorted(stc.keys()):
-        if stc[key] > 0:
+        if stc[key] != 0:
             print("{}: {}".format(key, stc[key]))
     raise
