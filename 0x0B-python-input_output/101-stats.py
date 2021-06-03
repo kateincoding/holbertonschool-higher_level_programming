7954558bc0820b9a71c41c1f573ed10b4d9847e7
kateincoding <kateincoding@gmail.com>
2021-06-03 03:32:36 +0000

    adding constructor

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,38 +2,38 @@
 """Module to write a script of dict of Filesize and code_status"""
 import sys
 
+if __name__ == '__main__':
+    filesize = 0
+    count = 0
+    stc = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+           "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            line = line.split()
 
-filesize = 0
-count = 0
-stc = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-try:
-    for line in sys.stdin:
-        line = line.split()
+            try:
+                filesize += int(line[-1])
+            except Exception:
+                pass
 
-        try:
-            filesize += int(line[-1])
-        except Exception:
-            pass
+            try:
+                code = line[-2]
+                if code in list(stc.keys()):
+                    stc[code] += 1
+            except Exception:
+                pass
 
-        try:
-            code = line[-2]
-            if code in list(stc.keys()):
-                stc[code] += 1
-        except Exception:
-            pass
+            count += 1
 
-        count += 1
+            if count % 10 == 0:
+                print("File size: {:d}".format(filesize))
+                for key in sorted(stc.keys()):
+                    if stc[key] != 0:
+                        print("{}: {:d}".format(key, stc[key]))
 
-        if count % 10 == 0:
-            print("File size: {:d}".format(filesize))
-            for key in sorted(stc.keys()):
-                if stc[key] != 0:
-                    print("{}: {:d}".format(key, stc[key]))
-
-except KeyboardInterrupt:
-    print("File size: {:d}".format(filesize))
-    for key in sorted(stc.keys()):
-        if stc[key] != 0:
-            print("{}: {:d}".format(key, stc[key]))
-    raise
+    except KeyboardInterrupt:
+        print("File size: {:d}".format(filesize))
+        for key in sorted(stc.keys()):
+            if stc[key] != 0:
+                print("{}: {:d}".format(key, stc[key]))
+        raise
