d90fb730c0dfa28535ce2698641de35e3269bf2d
kateincoding <kateincoding@gmail.com>
2021-06-05 21:36:45 +0000

    base + initializer

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for the class Base"""
+
+
+class Base:
+    """Class Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
