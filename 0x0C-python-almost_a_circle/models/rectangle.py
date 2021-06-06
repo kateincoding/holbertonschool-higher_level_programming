8ba6a4fb95444c09c9acb32fcad948492333263d
kateincoding <kateincoding@gmail.com>
2021-06-06 02:50:12 +0000

    class Rectangle finished until args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -34,6 +38,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
+        if type(value) is not int:
+            raise TypeError("heigth must be an integer")
+        if value <= 0:
+            raise ValueError("heigth must be > 0")
         self.__height = value
 
     @property
@@ -44,6 +52,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -53,5 +65,37 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """x setter"""
+        """y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Method to calculate the area of rectangle"""
+        return(self.width * self.height)
+
+    def display(self):
+        """Method to display a square of #"""
+        for y in range(0, self.y):
+            print()
+        for j in range(0, self.height):
+            for x in range(0, self.x):
+                print(" ", end="")
+            for i in range(0, self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """overriding to returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        string = "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"
+        string = string.format(self.id, self.x, self.y, self.width, self.height)
+        return string
+
+    def update(self, *args):
+        """that assigns an argument to each attribute"""
+        args_list = ["id", "width", "height", "x", "y"]
+        args_key = {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}
+        for count in range(0,args):
+            
