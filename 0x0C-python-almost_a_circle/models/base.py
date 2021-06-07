3be85f351463bdd1166351766dcdb884eae613aa
kateincoding <kateincoding@gmail.com>
2021-06-07 01:54:55 +0000

    class base mandatory finished

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for the class Base"""
+import json
 
 
 class Base:
@@ -14,3 +15,59 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Method that returns the JSON string representation of list_dictionaries
+        - list_dictionary: must be a list of dictionary
+        - Return: string of dictionary in json format"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            string = json.dumps(list_dictionaries)
+            return string
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Method that writes the JSON string
+        representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        result = []
+        if list_objs:
+            for objs in list_objs:
+                dictionary = objs.to_dictionary()
+                result.append(dictionary)
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(result))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Method that returns the list of the JSON
+        string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Method that returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Method that returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        result = []
+        try:
+            with open(filename, encoding="utf-8") as file:
+                obj_list = cls.from_json_string(file.read())
+                for dictionary in obj_list:
+                    result.append(cls.create(**dictionary))
+                return result
+        except:
+            return result
