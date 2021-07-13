a4f42e9126d008599898684953ae5dcd76bf6428
kateincoding <kateincoding@gmail.com>
2021-07-13 04:21:17 +0000

    fixing, adding sorting

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- script that lists all records of a table second_table
 -- database hbtn_0c_0 in your MySQL server
-SELECT score, name FROM second_table;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC, name;
