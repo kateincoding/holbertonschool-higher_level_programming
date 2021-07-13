a6ad748664b75909aeac3d76ff5e475b4ef61e72
kateincoding <kateincoding@gmail.com>
2021-07-13 04:13:53 +0000

    script to create a new table and set values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- script that creates a table second_table
+-- database hbtn_0c_0 in your MySQL server
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
