1bcdf9b8349f2fa7349fd73e8afda86b411c5410
kateincoding <kateincoding@gmail.com>
2021-07-14 04:57:47 +0000

    else

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+SELECT sh.title, shgr.genre_id FROM tv_shows sh LEFT JOIN tv_show_genres shgr
+ON sh.id=shgr.show_id WHERE shgr.genre_id IS NULL ORDER BY sh.title, shgr.genre_id ASC;
