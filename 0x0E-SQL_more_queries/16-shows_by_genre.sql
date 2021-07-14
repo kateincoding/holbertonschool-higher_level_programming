25451c6eec1c0b4af15feedfb0938c468229e6b9
kateincoding <kateincoding@gmail.com>
2021-07-14 05:31:03 +0000

    finished

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- script that lists all shows, and all genres linked to that show, from the databas
+SELECT sh.title, gr.name FROM tv_shows AS sh LEFT JOIN tv_show_genres AS shgr
+ON sh.id=shgr.show_id LEFT JOIN tv_genres AS gr
+ON shgr.genre_id=gr.id ORDER BY sh.title, gr.name ASC;
