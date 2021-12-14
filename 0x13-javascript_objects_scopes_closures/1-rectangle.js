691d780a8858e0a3b9772c3b95bfcb1d8fc30dde
kateincoding <kateincoding@gmail.com>
2021-12-14 18:32:18 -0500

    [create a rectangle]

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle:
+
+class Rectangle {
+  constructor (w, h) {
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
