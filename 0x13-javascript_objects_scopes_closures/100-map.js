f4a3a6136b39ede5491dc0bbf201b0ed4506e235
kateincoding <kateincoding@gmail.com>
2021-12-14 23:18:25 -0500

    [using map]

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,9 +2,6 @@
 // script that imports an array and computes a new array
 const { list } = require('./100-data');
 
-const newList = [];
-for (let i = 0; i < list.length; i++) {
-  newList.push(i * list[i]);
-}
+const newList = list.map((x, index) => x * index);
 console.log(list);
 console.log(newList);
