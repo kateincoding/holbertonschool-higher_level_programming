87514765e795e5404d383f2b9b043aaa92c17f87
kateincoding <kateincoding@gmail.com>
2021-12-14 22:51:40 -0500

    [console log , minim code]

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 // function that prints the number of arguments already printed and the new argument value
-
-exports.logMe = (function (item) {
-  let n = 0;
-  function print (item) {
-    console.log(`${n}: ${item}`);
-    n++;
-  }
-  return print;
-}());
+let counter = 0;
+exports.logMe = function (item) {
+  return console.log(`${counter++}: ${item}`);
+};
