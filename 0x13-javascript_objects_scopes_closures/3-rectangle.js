031e0e2eeae6d5758c04b9043548d8208d88fc13
kateincoding <kateincoding@gmail.com>
2021-12-14 21:26:51 -0500

    [print square]

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle with a side > 0
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
