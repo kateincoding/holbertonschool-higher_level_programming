2274e97c6135db67556f9e96c0e1098ddd906374
kateincoding <kateincoding@gmail.com>
2021-12-14 22:14:59 -0500

    [occurrences]

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// search a num of occurrences of a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      n++;
+    }
+  }
+  return n;
+};
