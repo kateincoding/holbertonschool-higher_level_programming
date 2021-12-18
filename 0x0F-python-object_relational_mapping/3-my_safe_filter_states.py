7d5c3ebf2dd300a5ec4f037f4ccd75dcdd83c4d7
kateincoding <kateincoding@gmail.com>
2021-12-18 17:15:34 -0500

    [sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,11 +3,8 @@ from sys import argv
 import MySQLdb;
 
 if __name__ == '__main__':
-    try:
-        db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-        state = argv[4]
-    except:
-        print("Can't connect to database")
+    db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    state = argv[4]
     cur = db.cursor()
     sql = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
     params = (state,)
