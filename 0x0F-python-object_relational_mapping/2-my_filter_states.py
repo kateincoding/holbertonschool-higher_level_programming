57cd40dd3562d49a4ba8d8c7f030bfd467577073
kateincoding <kateincoding@gmail.com>
2021-12-17 23:57:34 -0500

    states filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb;
+
+if __name__ == '__main__':
+    try:
+        db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+        state = argv[4]
+    except:
+        print("Can't connect to database")
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state)
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
