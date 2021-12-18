8168db3281b61c5f01d40c68035f464123cfd82c
kateincoding <kateincoding@gmail.com>
2021-12-18 15:27:59 -0500

    my filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
 from sys import argv
-import MySQLdb;
+import MySQLdb
 
 if __name__ == '__main__':
-    try:
-        db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-        state = argv[4]
-    except:
-        print("Can't connect to database")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    state = argv[4]
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state)
+    sql = """SELECT * FROM states
+          WHERE name = '{}'
+          ORDER BY states.id ASC""".format(state)
     cur.execute(sql)
     query_rows = cur.fetchall()
 
