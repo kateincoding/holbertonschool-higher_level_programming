bdad6f76979460b7e9120f438130d9566f245bf5
kateincoding <kateincoding@gmail.com>
2021-12-18 14:21:03 -0500

    pypstyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,11 @@
 
 
 from sys import argv
-import MySQLdb;
+import MySQLdb
 
 if __name__ == '__main__':
-    try:
-        db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-    except:
-        print("Can't connect to database")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     sql = "SELECT * FROM states ORDER BY states.id ASC"
     cur.execute(sql)
