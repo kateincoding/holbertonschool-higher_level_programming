4b7219f4c570903e793012dcac90bc5b5ff999d4
kateincoding <kateincoding@gmail.com>
2021-12-17 19:16:35 -0500

    print states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,17 @@
 from sys import argv
 import MySQLdb;
 
-def mysqlconnect():
+if __name__ == '__main__':
     try:
         db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     except:
         print("Can't connect to database")
-        return 0
-    print("Connected")
     cur = db.cursor()
+    sql = "SELECT * FROM states ORDER BY states.id ASC"
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
     cur.close()
     db.close()
-
-mysqlconnect()
