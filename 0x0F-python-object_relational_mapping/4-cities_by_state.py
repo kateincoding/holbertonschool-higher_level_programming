a3fd0a32fb728ca85bdd61e1d8f9a4ff7d96f47c
kateincoding <kateincoding@gmail.com>
2021-12-18 09:53:40 -0500

    [sql merge tables]

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb;
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    sql = "SELECT cities.id cities.name states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER by cities.id ASC"
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
