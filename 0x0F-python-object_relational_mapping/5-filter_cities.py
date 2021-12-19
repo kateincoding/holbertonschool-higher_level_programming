ab6786a06048fab1ee7ab37454d45178205f374c
kateincoding <kateincoding@gmail.com>
2021-12-19 11:26:50 -0500

    [comments]

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,6 +5,9 @@ lists all cities of that state, using the database hbtn_0e_4_usa """
 
 from sys import argv
 import MySQLdb
+"""script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
+
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
