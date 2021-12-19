6bae227d5ce2222fb22bd43e89db3eca2e8c21ac
kateincoding <kateincoding@gmail.com>
2021-12-19 11:33:56 -0500

    [format change]

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,18 +14,14 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3], charset="utf8")
     state = argv[4]
     cur = db.cursor()
-    sql = """SELECT cities.id, cities.name
-          FROM cities LEFT JOIN states
+    sql = """SELECT cities.name
+          FROM cities JOIN states
           ON cities.state_id = states.id
-          WHERE states.name = %s ORDER by id ASC"""
+          WHERE states.name = %s ORDER by cities.id ASC"""
     params = (state,)
     cur.execute(sql, params)
     query_rows = cur.fetchall()
 
-    for row in query_rows:
-        if row != query_rows[-1]:
-            print(row[1], end=", ")
-        else:
-            print(row[1])
+    print(", ".join(row[0] for row in query_rows))
     cur.close()
     db.close()
