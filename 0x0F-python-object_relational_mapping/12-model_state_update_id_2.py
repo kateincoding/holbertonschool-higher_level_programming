6dbce88470d65294605a3faa6bf437dd32a0f231
kateincoding <kateincoding@gmail.com>
2021-12-20 11:04:48 -0500

    [filter]

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that lists all State objects"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
+    if state:
+        state.name = 'New Mexico'
+        session.commit()
+    session.close()
