25a9beff6be4e80004ace5729b54af9b40088be1
kateincoding <kateincoding@gmail.com>
2022-01-09 12:45:35 -0500

    change for args]

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -2,4 +2,4 @@
 # Bash script that sends a DELETE request to the URL passed
 # as the first argument and displays the body of the response
 
-curl -sI -X OPTIONS 0.0.0.0:5000/route_4 | grep "Allow" | cut -d ' ' -f2-   
+curl -sI -X OPTIONS $1 | grep "Allow" | cut -d ' ' -f2-   
