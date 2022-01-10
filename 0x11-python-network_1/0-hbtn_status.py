c46dddfd7e50afc30470ec1a9f268527ad01ee2c
kateincoding <kateincoding@gmail.com>
2022-01-09 20:38:07 -0500

    norm

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# script that fetches https://intranet.hbtn.io/status
+"""script that fetches https://intranet.hbtn.io/status"""
+
 
 if __name__ == "__main__":
     import urllib.request
