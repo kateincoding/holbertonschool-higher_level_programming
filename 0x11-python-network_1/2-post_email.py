aaca462cd3b2b610110e94fb1452cb90ad5da8f2
kateincoding <kateincoding@gmail.com>
2022-01-09 21:54:11 -0500

    [post email]

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request"""
+
+
+if __name__=="__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    values = {"email":sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        answer = response.read().decode('utf-8')
+        print(answer)
