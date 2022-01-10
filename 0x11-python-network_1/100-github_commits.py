0f3c2aceee766da4644214756105032ab1af8016
kateincoding <kateincoding@gmail.com>
2022-01-09 23:59:03 -0500

    fixed

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
