285ed39921f21666de803b86f9dba00abc9c7ffd
kateincoding <kateincoding@gmail.com>
2022-01-09 22:53:55 -0500

    [headers]

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that displays the value of the variable X-Request-Id in the response header"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
