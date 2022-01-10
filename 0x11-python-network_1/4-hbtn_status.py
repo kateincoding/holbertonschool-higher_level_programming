2d0b355ee3a2a898ae8c2330882fcdb40e3241d4
kateincoding <kateincoding@gmail.com>
2022-01-09 22:44:58 -0500

    [tab]

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     response = requests.get("https://intranet.hbtn.io/status")
     content = response.text
     print("Body response:")
-    print("    - type: {}".format(type(content)))
-    print("    - content: {}".format(content))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
