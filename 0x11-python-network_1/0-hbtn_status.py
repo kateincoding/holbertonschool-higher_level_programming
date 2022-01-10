50c32c780cc37b418de3d282d0fb2992f3aa8ad0
kateincoding <kateincoding@gmail.com>
2022-01-09 20:28:58 -0500

    type, content, utf8 content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as resp:
+        data = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf content: {}".format(data.decode("utf-8")))
