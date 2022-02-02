104b14e92ef26e3f3befd4c15d25679375169575
kate <kateincoding@gmail.com>
2022-02-01 21:56:10 -0500

    [read a html and save in a file]

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const filename = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    fs.writeFile(filename, body, function (err) {
+      if (err) throw err;
+      console.log('Document created');
+    });
+  }
+});
